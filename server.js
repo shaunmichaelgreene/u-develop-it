78bacd2337fcd28190264bc7780fd15e6a8c03e0
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-06-13 17:36:03 -0400

    end 12.2.3 sever set up with node

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//test route
+// app.get('/', (req, res) => {
+//     res.json({
+//         message: 'Helloe World!'
+//     });
+// });
+
+//default response for not found error
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
