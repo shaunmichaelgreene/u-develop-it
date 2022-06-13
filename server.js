c4dc139406c7a2406a43e0fee190229aa9a70b24
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-06-13 18:38:29 -0400

    mid 12.2.4 1st db call built

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 //express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'shauncodes1986',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
 
 //test route
 // app.get('/', (req, res) => {
@@ -14,6 +26,11 @@ app.use(express.json());
 // });
 
 //default response for not found error
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
 app.use((req, res) => {
     res.status(404).end();
 });
