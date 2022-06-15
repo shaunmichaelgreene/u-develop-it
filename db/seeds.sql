50c202c294d06c3016321520c9879f2749afeaf4
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-06-15 18:28:22 -0400

    12.4.3 voters table added

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,12 @@
 INSERT INTO parties (name, description)
-VALUES 
-  ('JS Juggernauts', 'The JS Juggernauts eat, breathe, and sleep JavaScript. They can build everything you could ever want in JS, cluding a new kitchen sink.'),
-  ('Heroes of HTML', 'Want to see a mock-up turn into an actual webpage in a matter of minutes? Well, the Heroes of HTML can gett done  a matter of seconds.'),
+VALUES
+  ('JS Juggernauts', 'The JS Juggernauts eat, breathe, and sleep JavaScript. They can build everything you could ever want in JS, including a new kitchen sink.'), 
+  ('Heroes of HTML', 'Want to see a mock-up turn into an actual webpage in a matter of minutes? Well, the Heroes of HTML can get it done in a matter of seconds.'), 
   ('Git Gurus', 'Need to resolve a merge conflict? The Git Gurus have your back. Nobody knows Git like these folks do.');
 
-  INSERT INTO candidates (first_name, last_name, party_id, industry_connected)
-  VALUES
-    ('Ronald', 'Firbank', 1, 1),
+INSERT INTO candidates (first_name, last_name, party_id, industry_connected)
+VALUES
+  ('Ronald', 'Firbank', 1, 1),
   ('Virginia', 'Woolf', 1, 1),
   ('Piers', 'Gaveston', 1, 0),
   ('Charles', 'LeRoi', 2, 1),
@@ -16,3 +16,4 @@ VALUES
   ('Montague', 'Summers', 3, 1),
   ('Octavia', 'Butler', 3, 1),
   ('Unica', 'Zurn', NULL, 1);
+  
