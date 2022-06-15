b469c21388bc312555e7880efbf58048c11e821a
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-06-15 18:59:35 -0400

    start 12.4.4 modularizing started

---
+++ db/connection.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql2");
+
+// Connect to database
+const db = mysql.createConnection({
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password 
+    password: 'shauncodes1986',
+    database: 'election'
+  });
+
+  module.exports = db;
