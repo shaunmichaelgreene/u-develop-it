c14192460afd130d7ed47961a5db22cb40a0e2bb
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-06-20 19:34:50 -0400

    end 12.5.4 vote route added

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
   // Your MySQL username,
   user: 'root',
   // Your MySQL password
-  password: '',
+  password: 'shauncodes1986',
   database: 'election'
 });
 
